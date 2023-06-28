9beff0ca7e8be3d6f8b2dda67af8fa7030f439ce
David Celaya-Gonzalez <djcelaya@arizona.edu>
2023-06-28 09:34:25 -0700

    Typo in field names

--- uahs_campus_connect_migration/src/Plugin/migrate/source/PhotoGalleryImages.php
+++ uahs_campus_connect_migration/src/Plugin/migrate/source/PhotoGalleryImages.php
@@ -16,8 +16,8 @@ class PhotoGalleryImages extends SqlBase {
         $query->addField('n', 'nid', 'nid');
         $query->addField('fm', 'filename', 'filename');
         $query->addField('fm', 'uri', 'uri');
-        $query->addField('fdfi', 'field_image_alt', 'alt');
-        $query->addField('fdfi', 'field_image_title', 'title');
+        $query->addField('fdfi', 'field_images_alt', 'alt');
+        $query->addField('fdfi', 'field_images_title', 'title');
         $query->addField('fdfi', 'delta', 'delta');
         $query->addJoin('LEFT OUTER', 'field_data_field_images', 'fdfi', 'fdfi.entity_id = n.nid');
         $query->addJoin('LEFT OUTER', 'file_managed', 'fm', 'fm.fid = fdfi.field_images_fid');
