4119884aa38b9a233c105ed630fab7900a5f0757
David Celaya-Gonzalez <djcelaya@arizona.edu>
2023-07-05 09:57:24 -0700

    Remove DISTINCT from URIs to get right ordering

--- uahs_campus_connect_migration/src/Plugin/migrate/source/PhotoGalleryImagesGallery.php
+++ uahs_campus_connect_migration/src/Plugin/migrate/source/PhotoGalleryImagesGallery.php
@@ -3,6 +3,7 @@
 namespace Drupal\uahs_campus_connect_migration\Plugin\migrate\source;
 
 use Drupal\migrate\Plugin\migrate\source\SqlBase;
+use Drupal\migrate\Row;
 
 /**
  * Source plugin for the photo gallery paragraph used in the AZ News content that 
@@ -15,7 +16,8 @@ class PhotoGalleryImagesGallery extends SqlBase {
     public function query() {
         $query = $this->select('node', 'n');
         $query->addField('n', 'nid', 'nid');
-        $query->addExpression('GROUP_CONCAT(DISTINCT fm.uri)', 'uris');
+        $query->addExpression('GROUP_CONCAT(fm.uri)', 'uris');
+        $query->addExpression('GROUP_CONCAT(fdfi.delta)', 'deltas');
         $query->addJoin('LEFT OUTER', 'field_data_field_images', 'fdfi', 'fdfi.entity_id = n.nid');
         $query->addJoin('LEFT OUTER', 'file_managed', 'fm', 'fm.fid = fdfi.field_images_fid');
         $query->condition('n.type', 'photo_gallery', '=');
@@ -27,7 +29,8 @@ class PhotoGalleryImagesGallery extends SqlBase {
     public function fields() {
         return [
             'nid' => $this->t('Node ID'),
-            'uris' => $this->t('Image URIs')
+            'uris' => $this->t('Image URIs'),
+            'deltas' => $this->t('Image Deltas')
         ];
     }
 
@@ -38,5 +41,24 @@ class PhotoGalleryImagesGallery extends SqlBase {
                 'alias' => 'n'
             ]
         ];
+    }
+
+    public function prepareRow(Row $row) {
+        $uris_source = $row->getSourceProperty('uris');
+        $uris = explode(',', $uris_source);
+        $deltas_source = $row->getSourceProperty('deltas');
+        $deltas = explode(',', $deltas_source);
+        $uris_deltas = [];
+        for ($i = 0; $i < count($uris); $i++) {
+            $uri = $uris[$i];
+            $delta = $deltas[$i];
+            $uris_deltas[] = [
+                'uri' => $uri,
+                'delta' => $delta
+            ];
+        }
+        print_r($uris_deltas);
+        $row->setSourceProperty('uris_deltas', $uris_deltas);
+        return parent::prepareRow($row);
     }
 }
