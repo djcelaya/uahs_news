1470b7e7782fb52116ad8ba948296d17adbd3be0
David Celaya-Gonzalez <djcelaya@arizona.edu>
2023-07-14 14:59:42 -0700

    Remaining news release fields

--- uahs_campus_connect_migration/src/Plugin/migrate/source/sql/news_release.sql
+++ uahs_campus_connect_migration/src/Plugin/migrate/source/sql/news_release.sql
@@ -11,9 +11,18 @@ SELECT
 	fesl.field_external_story_link_url AS field_external_story_link_url,
 	fesl.field_external_story_link_title AS field_external_story_link_title,
 	b.body_value AS body,
-	
+	GROUP_CONCAT(DISTINCT fm2.uri) AS field_image,
+	GROUP_CONCAT(DISTINCT fm3.uri) AS field_files,
+	fdfrpg.field_related_photo_gallery_target_id AS field_related_photo_gallery,
+	fdfrv.field_related_video_target_id AS field_related_video,
 	fe.field_experts_value AS field_experts,
 	fc.field_contact_value AS field_contact,
+	fdfepkl.field_electronic_press_kit_link_title AS field_electronic_press_kit_link_title,
+	fdfepkl.field_electronic_press_kit_link_url AS field_electronic_press_kit_link_url,
+	fdfrns.field_related_news_stories_title AS field_related_news_stories_title,
+	fdfrns.field_related_news_stories_url AS field_related_news_stories_url,
+	fdfab.field_additional_boilerplates_value AS field_additional_boilerplates,
+	fdfrvr.field_related_video_release_target_id AS field_related_video_release,
 	GROUP_CONCAT(DISTINCT t_hsc2.name) AS field_health_sciences_category_2,
 	t_pc.name AS portal_category,
 	GROUP_CONCAT(DISTINCT t_a.name) AS field_affiliation,
@@ -35,13 +44,23 @@ LEFT JOIN field_data_field_youtube_video_id AS fdfyvi ON fdfyvi.entity_id = n.ni
 LEFT JOIN field_data_field_video AS fdfv ON fdfv.entity_id = n.nid
 
 JOIN field_data_field_post_date AS fpd ON fpd.entity_id = n.nid
-LEFT JOIN field_data_body AS b ON b.entity_id = n.nid
 LEFT JOIN field_data_field_external_story_link AS fesl ON fesl.entity_id = n.nid
+LEFT JOIN field_data_body AS b ON b.entity_id = n.nid
 
+LEFT JOIN field_data_field_image AS fi ON fi.entity_id = n.nid
+LEFT JOIN file_managed AS fm2 ON fm2.fid = fi.field_image_fid
+
+LEFT JOIN field_data_field_files AS ff ON ff.entity_id = n.nid
+LEFT JOIN file_managed AS fm3 ON fm3.fid = ff.field_files_fid
+
+LEFT JOIN field_data_field_related_photo_gallery AS fdfrpg ON fdfrpg.entity_id = n.nid
+LEFT JOIN field_data_field_related_video AS fdfrv ON fdfrv.entity_id = n.nid
 LEFT JOIN field_data_field_experts AS fe ON fe.entity_id = n.nid
 LEFT JOIN field_data_field_contact AS fc ON fc.entity_id = n.nid
-
-
+LEFT JOIN field_data_field_electronic_press_kit_link AS fdfepkl ON fdfepkl.entity_id = n.nid
+LEFT JOIN field_data_field_related_news_stories AS fdfrns ON fdfrns.entity_id = n.nid
+LEFT JOIN field_data_field_additional_boilerplates AS fdfab ON fdfab.entity_id = n.nid
+LEFT JOIN field_data_field_related_video_release AS fdfrvr ON fdfrvr.entity_id = n.nid
 
 LEFT JOIN field_data_field_health_sciences_category_2 AS fhsc2 ON fhsc2.entity_id = n.nid
 LEFT JOIN taxonomy_term_data AS t_hsc2 ON t_hsc2.tid = fhsc2.field_health_sciences_category_2_tid
