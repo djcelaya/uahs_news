c91835a34810e2c9288f6e57b8ba730a0e7dc8eb
David Celaya-Gonzalez <djcelaya@arizona.edu>
2023-07-20 05:57:12 -0700

    TI News video SQLs

--- uahs_campus_connect_migration/src/Plugin/migrate/source/sql/tih_news_import.sql
+++ uahs_campus_connect_migration/src/Plugin/migrate/source/sql/tih_news_import.sql
@@ -3,6 +3,7 @@ SELECT
 	n.title,
 	fm1.uri AS field_banner_image,
 	fm2.uri AS field_card_image,
+	fdfyvi.field_youtube_video_id_value AS field_youtube_video_id,
 	ft.field_teaser_value AS field_teaser,
 	fpd.field_post_date_value AS field_post_date,
 	b.body_value AS body,
@@ -19,6 +20,8 @@ LEFT JOIN file_managed AS fm1 ON fm1.fid = fbi.field_banner_image_fid
 
 LEFT JOIN field_data_field_card_image AS fci ON fci.entity_id = n.nid
 LEFT JOIN file_managed AS fm2 ON fm2.fid = fci.field_card_image_fid
+
+LEFT JOIN field_data_field_youtube_video_id AS fdfyvi ON fdfyvi.entity_id = n.nid
 
 LEFT JOIN field_data_field_contact AS fc ON fc.entity_id = n.nid
 
