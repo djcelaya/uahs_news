1e83b49519b6c263fce363da22d87701ce750559
David Celaya-Gonzalez <djcelaya@arizona.edu>
2023-07-25 05:24:10 -0700

    News release images SQL

---
+++ uahs_campus_connect_migration/src/Plugin/migrate/source/sql/news_release_images.sql
@@ -0,0 +1,20 @@
+SELECT
+	n.nid,
+	fm.filename AS filename,
+	fm.uri AS uri,
+	fi.field_image_alt AS field_image_alt,
+	fi.field_image_title AS field_image_title
+
+FROM node AS n
+
+LEFT JOIN field_data_field_image AS fi ON fi.entity_id = n.nid
+JOIN file_managed AS fm ON fm.fid = fi.field_image_fid
+
+WHERE
+	n.type = 'news_release'
+	AND n.status = 1
+	AND fm.type = 'image'
+
+GROUP BY fm.fid
+
+ORDER BY fm.timestamp DESC
