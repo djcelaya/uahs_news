bb42bdc43815341560bd9d6d320a25653ac096ef
David Celaya-Gonzalez <djcelaya@arizona.edu>
2023-09-11 12:28:58 -0700

    Call out inline images SQL

---
+++ uahs_campus_connect_migration/src/Plugin/migrate/source/sql/newsstory_call_out_images.sql
@@ -0,0 +1,20 @@
+SELECT
+	n.nid,
+	fm.filename AS filename,
+	fm.uri AS uri,
+	fi.field_images_alt AS field_image_alt,
+	fi.field_images_title AS field_image_title
+
+FROM node AS n
+
+LEFT JOIN field_data_field_images AS fi ON fi.entity_id = n.nid
+JOIN file_managed AS fm ON fm.fid = fi.field_images_fid
+
+WHERE
+	n.type = 'hsc_news_call_out'
+	AND n.status = 1
+	AND fm.type = 'image'
+
+GROUP BY fm.fid
+
+ORDER BY fm.timestamp DESC
